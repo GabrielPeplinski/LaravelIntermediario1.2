7e8f10c9706f4cb150241a0831565bc473e3a1f2
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2022-05-05 16:18:14 -0300

    Unused code removed

--- app/Actions/Borrow/DeleteBorrowAction.php
+++ app/Actions/Borrow/DeleteBorrowAction.php
@@ -9,9 +9,6 @@ class DeleteBorrowAction
 {
     public function execute(Borrow $borrow):bool
     {
-//        $otherBook = new Book();
-//        $borrow->setRelation('book', $otherBook);
-
         app(MakeBookAvailableAction::class)->execute($borrow->book);
 
         return $borrow->delete();
